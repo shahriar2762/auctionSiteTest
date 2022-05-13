946707a3ca0cd0d81870e7ace6c7972dba57dc4a
shahriar2762 <shahriar2762@gmail.com>
2022-05-14 03:32:30 +0600

    1st commit

--- src/main/java/com/example/auctionSiteTest/MainPage.java
+++ src/main/java/com/example/auctionSiteTest/MainPage.java
@@ -7,6 +7,5 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class MainPage {
     public SelenideElement seeAllToolsButton = $("a.wt-button_mode_primary");
-    public SelenideElement toolsMenu = $x("//div[contains(@class, 'menu-main__item') and text() = 'Tools']");
-    public SelenideElement searchButton = $("[data-test=menu-main-icon-search]");
+
 }
